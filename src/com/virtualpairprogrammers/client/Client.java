18be4235c9e828c97ea781a0c5b63fa8a8ff6283
KamilOgonowski <kml.ogonowski@gmail.com>
2023-12-28 00:18:02 +0100

    First mocking run - beans have been created

--- src/com/virtualpairprogrammers/client/Client.java
+++ src/com/virtualpairprogrammers/client/Client.java
@@ -1,8 +1,24 @@
 package com.virtualpairprogrammers.client;
 
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.virtualpairprogrammers.domain.Customer;
+import com.virtualpairprogrammers.services.customers.CustomerManagementService;
+
 public class Client {
 
 	public static void main(String[] args) {
+		ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
+		CustomerManagementService customerService = container.getBean("CustomerMock", CustomerManagementService.class );
+		List<Customer> allCustomers = customerService.getAllCustomers();
+		
+		for(Customer next : allCustomers) {
+			System.out.println(next);
+		}
+		
+		container.close();
 		
 
 	}
